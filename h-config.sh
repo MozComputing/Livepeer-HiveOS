7a74d23be4a59844e16df866cba56aece0febbfd
MozComputing <hello@mozcomputing.dev>
2023-03-20 21:34:49 +0100

    feat: add default values

--- h-config.sh
+++ h-config.sh
@@ -7,6 +7,8 @@ conf="${CUSTOM_USER_CONFIG}"
 [[ ! -z $CUSTOM_URL ]] && conf="$conf -orchAddr $CUSTOM_URL"
 [[ ! -z $CUSTOM_PASS ]] && conf="$conf -orchSecret $CUSTOM_ORCHESTRATOR_SECRET"
 [[ ! -z $CUSTOM_PASS ]] && echo "$CUSTOM_PASS" > "$CUSTOM_ORCHESTRATOR_SECRET"
+[[ ! $conf =~ "-trasncoder" ]] && conf="$conf -transcoder"
+[[ ! $conf =~ "-nvidia" ]] && conf="$conf -nvidia all"
 
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
 echo "$conf" > "$CUSTOM_CONFIG_FILENAME"
