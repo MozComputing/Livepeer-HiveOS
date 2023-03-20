e1c9c580b7e0c275d205bd07a2152cdaad4a50b6
MozComputing <hello@mozcomputing.dev>
2023-03-20 21:34:32 +0100

    feat: add native hiveOS field

--- h-config.sh
+++ h-config.sh
@@ -4,5 +4,9 @@
 
 conf="${CUSTOM_USER_CONFIG}"
 
+[[ ! -z $CUSTOM_URL ]] && conf="$conf -orchAddr $CUSTOM_URL"
+[[ ! -z $CUSTOM_PASS ]] && conf="$conf -orchSecret $CUSTOM_ORCHESTRATOR_SECRET"
+[[ ! -z $CUSTOM_PASS ]] && echo "$CUSTOM_PASS" > "$CUSTOM_ORCHESTRATOR_SECRET"
+
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
 echo "$conf" > "$CUSTOM_CONFIG_FILENAME"
